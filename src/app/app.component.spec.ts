49eaa7721514bf09dc645a5d4f9612a1e6862b71
JulianLargo72 <julianbetancur104@gmail.com>
2026-06-21 12:07:04 -0500

    actualizar spect

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Catalogo Virtual' title`, () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Catalogo Virtual');
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.textContent).toContain('Catalogo Virtual');
   });
 
   it('should render title', () => {
